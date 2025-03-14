24a001772815aedbee73ef357ba1cac312209263
Ogennaisrael <ogennaisrael@gmail.com>
2025-03-14 07:57:06 +0100

    Update My_Repository.py
    Add to repository

--- My_Repository.py
+++ My_Repository.py
@@ -1,6 +1,8 @@
 def greet_user(name):
 
-    return (f"Hello, {name.capitalize()}")
+    print (f"Hello, {name.capitalize()}")
+    for i in name:
+        return i
 
 greet_user("ogenna israel")
 greet_user("ihechi festus")
